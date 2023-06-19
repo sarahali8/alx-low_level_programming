0d5c49dc6fee6e9e3128816e88e30dbdd81f57b8
sarahali8 <elsawahsarah08@gmail.com>
2023-06-19 15:15:07 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#iclude <stdio.h>
+/**
+ * main A program that prints a line with puts function
+ *Retrun: 0 (success)
+ */
+int main (void)
+{
+	puts ("\"programing is like bullding a mulltilingual puzzle");
+	return (0);
+}
