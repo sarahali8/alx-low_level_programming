98877535d33bd36cdb98095f11dbb29cf6cfcc3a
sarahali8 <elsawahsarah08@gmail.com>
2023-08-12 08:32:22 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,11 +7,15 @@
 int main(void)
 {
 	int i;
-	char j;
 	for (i = 0 ; i < 10 ; i++)
+	{
 		putchar(i + '0');
-	for (j = 'a' ; j <= 'f' ; j++)
-		putchar(j);
-	putchar('\n');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar ('\n');
 	return (0);
 }
