aec07a8ad75228a73280d284bd20ea5bee9a355b
sarahali8 <elsawahsarah08@gmail.com>
2023-08-17 10:31:41 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 to 9
+ * return void
+*/
+
+void print_numbers(void)
+{
+	int i;
+	for (i = 0 ; i < 10 ; i++)
+		_putchar(i + '0');
+	_putchar('\n');
+}
