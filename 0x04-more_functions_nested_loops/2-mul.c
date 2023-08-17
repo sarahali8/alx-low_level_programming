409366f0803e4f9dc4f3e0dc28697b97291ecfdf
sarahali8 <elsawahsarah08@gmail.com>
2023-08-17 10:08:32 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - check if c is upper
+ *
+ * @a:first input
+ * @b:second input
+ *
+ * Return: return results
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
