3ec212e51dd98e1077ebd1e21d18502bce1d80b1
sarahali8 <elsawahsarah08@gmail.com>
2023-08-21 16:47:15 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,7 +12,7 @@ void rev_string(char *s)
 	int count = 0;
 	for (i = 0 ; s[i] != '\0' ; i++)
 		count++;
-	for (i = 0 ; i > count / 2 ; i++)
+	for (i = 0 ; i < count / 2 ; i++)
 	{
 		char j;
 		j = s[i];
