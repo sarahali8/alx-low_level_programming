d6b1dcca52187156925a0446fd1d6cf95e78f06f
sarahali8 <elsawahsarah08@gmail.com>
2023-08-21 14:04:07 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * _strlen - retun the length of a string
+ *
+ * @s: string to be counted
+ *
+ * Returun: length of the sting
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int count = 0;
+	for (i = 0 ; s[i] != '\0' ; i++)
+		count++;
+	return (count);
+}
