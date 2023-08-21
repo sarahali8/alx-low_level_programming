45a13ec4ef25a2df005c4403a908dc8763d3a594
sarahali8 <elsawahsarah08@gmail.com>
2023-08-21 16:41:54 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * rev_string - prints string in reverse
+ * @s: string to be printed
+ * Returun: void
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	int count = 0;
+	for (i = 0 ; s[i] != '\0' ; i++)
+		count++;
+	for (i = 0 ; i > count / 2 ; i++)
+	{
+		char j;
+		j = s[i];
+		s[i] = s[count - 1 - i];
+		s[count - 1 - i] = j;
+	}
+}
