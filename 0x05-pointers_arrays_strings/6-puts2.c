0e3a8a44277f18c41929813fbe15fe48813913d6
sarahali8 <elsawahsarah08@gmail.com>
2023-08-21 17:23:10 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: string to be printed
+ * Returun: void
+ */
+void puts2(char *str)
+{
+	int i;
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		if (i % 2 == 0)
+			putchar(str[i]);
+	}
+	putchar('\n');
+}
