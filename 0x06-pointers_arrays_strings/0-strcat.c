35c0d012fb8189cd6b49fed90f4d7c57d677cfaa
sarahali8 <elsawahsarah08@gmail.com>
2023-08-24 14:21:32 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -17,6 +17,6 @@ char *_strcat(char *dest, char *src)
 	for (i = 0 ; src[i] != '\0' ; i++)
 		srclen++;
 	for (i = 0 ; i <= srclen ; i++)
-		dest[destlen - 1] = src[i];
+		dest[destlen + 1] = src[i];
 	return (dest);
 }
