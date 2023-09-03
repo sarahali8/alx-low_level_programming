0e4a6d9e29ed556b433cd087252b47359d755900
sarahali8 <elsawahsarah08@gmail.com>
2023-09-03 07:42:46 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,8 +7,8 @@
  * Return: always 0 (success)
  */
 
-int main(int argc, char * argv[])
+int main(int argc, char * argv[] _attribute_((unused)))
 {
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 	return (0);
 }
