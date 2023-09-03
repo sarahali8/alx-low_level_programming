295678e9b61a8371520c37bc467e67e09a462374
sarahali8 <elsawahsarah08@gmail.com>
2023-09-03 07:18:45 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -prints the name of the program
+ * @argc: number of arguments
+ * @argv: number of arguments
+ * Returun: always 0 (success)
+ */
+
+int main(char *argv[])
+{
+	printf("%s\n", argv[0]);
+		return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -prints number of argment
+ * @argc:  arguments count
+ * @argv:  arguments vector
+ * Return: always 0 (success)
+ */
+
+int main(int argc, char * argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
